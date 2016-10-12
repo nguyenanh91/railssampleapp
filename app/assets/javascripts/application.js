941975869da8877adcd3aa5764273b10b421ce13
Nguyen Hoang Anh <nguyen.hoang.anh@framgia.com>
2016-10-12 17:16:26 +0700

    Add Login function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require turbolinks
 //= require_tree .
 //= require bxslider
+//= require bootstrap
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get    '/login',   to: 'sessions#new'
+
+  post   '/login',   to: 'sessions#create'
+
+  delete '/logout',  to: 'sessions#destroy'
+
   get 'users/index'
 
   get 'users/new'
